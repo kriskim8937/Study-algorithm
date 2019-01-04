73c1d1892ba59bd2139b9a9bdf0563ff80e38ad9
kriskim8937 <40317107+kriskim8937@users.noreply.github.com>
2019-01-04 22:59:45 +0900

    Create Baekjoon2741.cpp

---
+++ Baekjoon2741.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	scanf_s("%d", &num, sizeof(num));
+	for (int i=1; i <= num; i++)
+	{
+		printf("%d\n", i);
+	}
+	
+
+	return 0;
+
+}
+/*
+작이지는 수 차례로 
+*/
