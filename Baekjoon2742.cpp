a017a28345c3a7d8a5b9204c3087f7d5942e3e64
kriskim8937 <40317107+kriskim8937@users.noreply.github.com>
2019-01-04 23:02:25 +0900

    Create Baekjoon2742.cpp

---
+++ Baekjoon2742.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	scanf_s("%d", &num, sizeof(num));
+	for (num; num  > 0; num--)
+	{
+		printf("%d\n", num);
+	}
+	
+
+	return 0;
+
+}
