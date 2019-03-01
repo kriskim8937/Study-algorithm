7ab9869a2f658189aa8934ccd5af8e18809d22c3
kriskim8937 <40317107+kriskim8937@users.noreply.github.com>
2019-03-01 16:20:27 +0900

    Update 1303.py

--- BFS/1303.py
+++ BFS/1303.py
@@ -1,16 +1,18 @@
+###1303 전쟁-
+
 import sys
-from collections import deque
+from collections import deque ##중요포인트
 read = lambda : sys.stdin.readline().strip()
 N,M = map(int, read().split())
 matrix = [list(map(str,[*read()])) for _ in range(M)]
-check = [list([int(0)]*N) for _ in range(M)]
+check = [list([int(0)]*N) for _ in range(M)] ##중요포인트
 count= [0,0]#W,B
 
 def bfs(i,j):
     
     dro = [1,-1,0,0]
     dco = [0,0,1,-1]
-    q = deque([(i,j)])
+    q = deque([(i,j)])##중요포인트 
     t = 1
     check[i][j] = 1
     while q:
@@ -22,8 +24,7 @@ def bfs(i,j):
                 if matrix[nro][nco] == matrix[ro][co]:
                     
                     if check[nro][nco] == 0:
-                        check[nro][nco] = 1 
-                        
+                        check[nro][nco] = 1                         
                         q.append((nro,nco))
                         t+=1
     return t
@@ -36,3 +37,5 @@ for j in range(N):
             else: B+=ans**2
 
 print(W,B)
+#주의 할점  문자열 비교할때는 'W'
+#return t를 시켜서 그걸 계속 더하는방식
